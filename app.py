220ae03b24f88c136e91b21a1d3d4519f21927e0
Enip Yu <a23956491z@yahoo.com.tw>
2021-12-10 13:55:11 +0800

    update website UI

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import numpy as np
 import config
 from werkzeug.utils import secure_filename
 
+import os
 from model import Pytorch_model
 
 app = Flask(__name__)
@@ -17,7 +18,20 @@ app.config.from_object(config)
 photos = UploadSet('PHOTO')
 configure_uploads(app, photos)
 
+def open_resize(img_path):
 
+    store_size = 512
+    img_file = os.path.join('uploads', img_path)
+
+    img = cv2.imread(img_file)
+
+    biggest = np.max(img.shape)
+    scale_ratio = biggest / store_size
+    print()
+    resized = cv2.resize(img, (int(img.shape[1]//scale_ratio), int(img.shape[0]//scale_ratio)))
+
+    cv2.imwrite(img_file, resized)
+    return img_path
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,14 +39,24 @@ def index():
 
 @app.route('/demo', methods=['POST', 'GET'])
 def upload():
+
     if request.method == 'POST':
         img = request.files['img'].filename
         topk = request.form['topk']
         img = secure_filename(img)
+
+        if 'JPEG' in img:
+            img = img.replace("JPEG", 'jpg')
+        print(img)
         new_name = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + '_' + img
+
         filename = photos.save(request.files['img'], name=new_name)
+
+        open_resize(filename)
+
         data = predict_img(photos.path(filename), is_numpy=False,topk=int(topk))
         img_path = photos.url(filename)
+
         return flask.jsonify({"result":data,"img_path":img_path})
     else:
         img_path = None
@@ -73,8 +97,8 @@ if __name__ == '__main__':
     print("Loading PyTorch model and Flask starting server ...")
     print("Please wait until server has fully started")
 
-    model_path = 'cifar.pt'
+    model_path = 'feed.pt'
     gpu_id = 0
     model = Pytorch_model(model_path=model_path, img_shape=[
-        224, 224], img_channel=3, gpu_id=gpu_id, classes_txt='labels_cifar')
+        224, 224], img_channel=3, gpu_id=gpu_id, classes_txt='labels_feeds')
     app.run(host="0.0.0.0", port="3000")
